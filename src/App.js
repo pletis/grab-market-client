2401dd6821fa7abe7c0b839e0a0eb2cc7133e8e7
pletis <33502771+pletis@users.noreply.github.com>
2021-05-17 23:35:49 +0900

    상품 antd 기본 적용

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
+import 'antd/dist/antd.css';
 import './App.css';
 import MainPageComponent from './main/index.js';
 import UploadPage from "./upload"
 import ProductPage from "./product"
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Link, useHistory} from 'react-router-dom';
+import {Button} from "antd";
+import {DownloadOutlined} from "@ant-design/icons";
 
 function App() {
+    const history =useHistory();
     return (
         <div>
             <div id="header">
             <div id="header-area">
+                <Link to="/">
                 <img src="/images/icons/잇츠마인.png" width="150"/>
+                </Link>
+                <Button size="large" onClick={function(){
+                    history.push('/upload');
+                }}
+                icon={<DownloadOutlined/>}
+                >상품 업로드</Button>
             </div>
             </div>
             <div id="body">
