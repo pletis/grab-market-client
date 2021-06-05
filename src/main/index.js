1b78a2c0da73fca73de0bff223e2334241fdb850
pletis <33502771+pletis@users.noreply.github.com>
2021-06-06 00:02:26 +0900

    슬라이드배너&결제하기

--- src/main/index.js
+++ src/main/index.js
@@ -5,11 +5,13 @@ import {Link} from 'react-router-dom'
 import dayjs from "dayjs"
 import relativeTime from "dayjs/plugin/relativeTime"
 import {API_URL} from "../config/constants.js"
+import {Carousel} from 'antd';
 
 dayjs.extend(relativeTime);
 
 function MainPage() {
     const [products, setProducts] = React.useState([0]);
+    const [banners, setBanners] = React.useState([]);
     React.useEffect(
         function(){
         axios.get(`${API_URL}/products`)
@@ -20,19 +22,41 @@ function MainPage() {
         })
         .catch(error => {
         console.error("에러발생!!",error)
-    })
-        },[])
+        });
+
+        axios.get(`${API_URL}/banners`)
+        .then((result) => {
+            const banners = result.data.banners;
+            setBanners(banners);
+        })
+        .catch((error) => {
+            console.error("에러발생 : ",error)
+        })
+    },[])
     
     return ( 
     <div>
-            <div id="banner">
-                <img src="images/icons/banner1.png"/>
-            </div>
+        <Carousel autoplay autoplaySpeed={3000}>
+            {
+                banners.map((banner, index) => {
+                    return (
+                        <Link to={banner.href}>
+                            <div id="banner">
+                                <img src={`${API_URL}/${banner.imageUrl}`}/>
+                            </div>
+                        </Link>
+                    )
+                })
+            }
+        </Carousel>
             <h1 id="product-headline">판매상품</h1>
             <div id="product-list">
                 {products.map(function(product, index){
                         return (
                 <div className="product-card">
+                    {
+                        product.soldout === 1 && <div className="product-blur"/>
+                    }
                     <Link className="product-link" to={`/products/${product.id}`}>
                     <div>
                         <img className="product-img" src={`${API_URL}/${product.imageUrl}`}/>
--- src/product/index.js
+++ src/product/index.js
@@ -3,12 +3,14 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import "./index.css";
 import {API_URL} from "../config/constants"
-import dayjs from 'dayjs'
+import dayjs from 'dayjs';
+import {Button, message} from 'antd';
 
 function ProductPage(){
     const {id} = useParams();
     const [product, setProduct] = useState(null);
-    useEffect(function(){
+
+    const getProduct = () => {
         axios.get(`${API_URL}/products/${id}`)
         .then(result => {
             setProduct(result.data.product)
@@ -16,10 +18,25 @@ function ProductPage(){
         .catch(error => {
             console.error("에러!",error);
         });
+    }
+
+    useEffect(function(){
+        getProduct();
     },[])
 
     if(product===null){
         return <h1>상품 정보를 받고 있습니다...</h1>
+    }
+
+    const onClickPurchase = () => {
+        axios.post(`${API_URL}/purchase/${id}`)
+        .then((result) => {
+            message.info("구매가 완료되었습니다.")
+            getProduct()
+        })
+        .catch((error) => {
+            message.error(`에러가 발생했습니다. ${error.message}`)
+        })
     }
     
     return (
@@ -36,6 +53,15 @@ function ProductPage(){
                 <div id="name">{product.name}</div>
                 <div id="price">{product.price}원</div>
                 <div id="createdAt">{dayjs(product.createdAt).format('YYYY년 MM월 DD일')}</div>
+                <Button id="purchase-button"
+                size='large'
+                type="primary"
+                danger
+                onClick={onClickPurchase}
+                disabled={product.soldout === 1 ? true : false}
+                >
+                    재빨리 구매하기
+                </Button>
                 <pre id="description">{product.description}</pre>
             </div>
         </div>
